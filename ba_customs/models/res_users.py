5fe0b5978c20b64534639cdb72781fa5eed36c20
TLuigi1 <84993601+TLuigi1@users.noreply.github.com>
2022-03-28 09:09:36 -0600

    Delete ba_customs directory

--- ba_customs/models/res_users.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    nombramiento_elearning = fields.Char(string="Titulo")
-    firma_digital = fields.Binary(string="Firma digital")
-    karma = fields.Integer('Karma', default=10)
